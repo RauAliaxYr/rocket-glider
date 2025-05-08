3b3bfaa078987f82edf6eda13589a40a9918f03b
Aliaksandr Rau <rau.al.yr@gmail.com>
2025-05-08 15:30:19 +0300

    Refactor: Refactor Level Manager logic and Flight Tracker logic

--- Assets/Scripts/FlightTracker.cs
+++ Assets/Scripts/FlightTracker.cs
@@ -7,16 +7,28 @@ public class FlightTracker : MonoBehaviour
     public int coinsEarned { get; private set; }
 
     private Vector2 startPosition;
+    private bool levelAdvanced = false;
+    private LevelManager levelManager;
     
     void Start()
     {
         startPosition = transform.position;
+        levelAdvanced = false;
+        levelManager = LevelManager.Instance;
     }
 
     void Update()
     {
         distanceTravelled = transform.position.x - startPosition.x;
         distanceTravelled = Mathf.Max(0f, distanceTravelled);
+        if (!levelAdvanced && levelManager != null)
+        {
+            if (distanceTravelled >= LevelManager.Instance.CurrentLevel.requiredDistanceToPass)
+            {
+                levelAdvanced = true;
+                LevelManager.Instance.TryAdvanceLevel(distanceTravelled);
+            }
+        }
     }
 
     public int TakeCoindByTravel()
