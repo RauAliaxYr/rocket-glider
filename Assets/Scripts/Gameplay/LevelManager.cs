43c9b09b0685c6e8ff2cc7097bd09b0df0e210ef
Aliaksandr Rau <rau.al.yr@gmail.com>
2025-06-12 00:08:54 +0300

    Refactoring: LevelManager is refactored

--- Assets/Scripts/Gameplay/LevelManager.cs
+++ Assets/Scripts/Gameplay/LevelManager.cs
@@ -6,60 +6,118 @@ public class LevelManager : MonoBehaviour
     public event Action OnLevelAdvanced;
     public static LevelManager Instance { get; private set; }
     public bool IsLevelReadyToAdvance { get; private set; } = false;
-    [Header("Level Data")]
-    [SerializeField] private LevelData[] levels;
-    private int currentLevelIndex = 0;
-    private const string LevelKey = "CurrentLevelIndex";
-
-    public LevelData CurrentLevel => levels[currentLevelIndex];
     
-    [Header("Audio Clips")]
-    [SerializeField] private AudioClip levelAudioClip ;
+    [Header("Level Settings")]
+    [SerializeField] private LevelData[] _levels;
+    [SerializeField] private AudioClip _levelMusic;
+    
+    private const string LEVEL_KEY = "CurrentLevelIndex";
+    private int _currentLevelIndex = 0;
+
+    public LevelData CurrentLevel => _levels[_currentLevelIndex];
+    public int CurrentLevelNumber => _currentLevelIndex + 1;
+    public int TotalLevels => _levels.Length;
+    
+    private void Awake()
+    {
+        InitializeSingleton();
+        LoadLevelProgress();
+    }
 
     private void Start()
     {
-        AudioManager.Instance.PlayMusic(levelAudioClip);
+        PlayLevelMusic();
     }
 
-    private void Awake()
+    #region Initialization
+    private void InitializeSingleton()
     {
-        if (Instance == null)
-        {
-            //ResetAllProgress();
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-            currentLevelIndex = PlayerPrefs.GetInt(LevelKey, 0);
-        }
-        else
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
+            return;
         }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
+    private void LoadLevelProgress()
+    {
+        _currentLevelIndex = PlayerPrefs.GetInt(LEVEL_KEY, 0);
+        ClampLevelIndex();
+    }
+
+    private void PlayLevelMusic()
+    {
+        if (_levelMusic != null && AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlayMusic(_levelMusic);
+        }
+    }
+    #endregion
+
+    #region Level Progression
     public void TryAdvanceLevel(float distanceTraveled)
     {
-        if (currentLevelIndex < levels.Length - 1 &&
-            distanceTraveled >= CurrentLevel.requiredDistanceToPass)
+        if (CanAdvanceLevel(distanceTraveled))
         {
-            IsLevelReadyToAdvance = true; // Только флаг
+            IsLevelReadyToAdvance = true;
         }
     }
 
-    public void ResetLevels()
-    {
-        currentLevelIndex = 0;
-    }
     public void ConfirmAdvanceLevel()
     {
         if (!IsLevelReadyToAdvance) return;
-        currentLevelIndex++;
+
+        _currentLevelIndex++;
+        ClampLevelIndex();
+        
+        SaveLevelProgress();
         IsLevelReadyToAdvance = false;
-        PlayerPrefs.SetInt(LevelKey, currentLevelIndex);
-        PlayerPrefs.Save();
-        OnLevelAdvanced?.Invoke(); // Сигнал для других систем
+        
+        OnLevelAdvanced?.Invoke();
     }
+
+    private bool CanAdvanceLevel(float distanceTraveled)
+    {
+        return _currentLevelIndex < _levels.Length - 1 && 
+               distanceTraveled >= CurrentLevel.requiredDistanceToPass;
+    }
+
+    private void ClampLevelIndex()
+    {
+        _currentLevelIndex = Mathf.Clamp(_currentLevelIndex, 0, _levels.Length - 1);
+    }
+    #endregion
+
+    #region Progress Management
+    private void SaveLevelProgress()
+    {
+        PlayerPrefs.SetInt(LEVEL_KEY, _currentLevelIndex);
+        PlayerPrefs.Save();
+    }
+
+    public void ResetLevelProgress()
+    {
+        _currentLevelIndex = 0;
+        SaveLevelProgress();
+    }
+
     public void ResetAllProgress()
     {
         PlayerPrefs.DeleteAll();
+        ResetLevelProgress();
     }
+    #endregion
+
+    #region Editor Helpers
+    // Для дебага в редакторе
+    public void DebugAdvanceLevel()
+    {
+        _currentLevelIndex++;
+        ClampLevelIndex();
+        Debug.Log($"Debug: Advanced to level {CurrentLevelNumber}");
+    }
+    #endregion
 }
